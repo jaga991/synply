7fa54a6e516dc3af4cf8d3613d4bda2a495c9514
Koh Chee Kiat <66861775+jaga991@users.noreply.github.com>
2024-01-12 16:05:56 +0800

    s

---
+++ src/app/make-event/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import Link from "next/link";
+import { Libre_Baskerville } from 'next/font/google';
+
+
+export default function MakeEvent() {
+  return (
+    <div className="flex flex-col">
+
+    </div>
+  )
+}
