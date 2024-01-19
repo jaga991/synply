ec68708f62aa4a4f809df19e810670893a3e678c
Koh Chee Kiat <66861775+jaga991@users.noreply.github.com>
2024-01-19 19:02:24 +0800

    create calendar and days of week

--- src/app/make-event/page.tsx
+++ src/app/make-event/page.tsx
@@ -4,11 +4,34 @@ import Link from "next/link";
 import { Libre_Baskerville } from 'next/font/google';
 import { useState } from 'react';
 import Header from '../header';
+import DatePicker from "react-multi-date-picker"
+import { Calendar } from "react-multi-date-picker"
 
+
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function MakeEvent() {
+  const [value, setValue] = useState<Value>(new Date());
   const [eventName, setEventName] = useState()
-  const [dayType, setDayType] = useState<String>()
+  const [dayType, setDayType] = useState<String>('day of week')
+  const [dayOfWeek, setDayOfWeek] = useState<string[]>([])
+
+  const handleDayOfWeek = (e: any) => {
+    if(!dayOfWeek.includes(e.target.name)) {
+      setDayOfWeek(
+        [...dayOfWeek, e.target.name]
+      )
+    }
+    else if(dayOfWeek.includes(e.target.name)) {
+      setDayOfWeek(dayOfWeek.filter(day => day != e.target.name))
+    }
+
+    console.log(dayOfWeek)
+    console.log(e.target.name)
+  }
 
   const handleEventChange = (e: any) => {
     console.log(e.target.value)
@@ -16,12 +39,23 @@ export default function MakeEvent() {
   }
 
   const handleDayChange = (e: any) => {
-    if(e.target.value == "Dates") {
+    console.log(e.target.name)
+    if(e.target.name == "Dates") {
+      console.log('dates')
       setDayType('dates')
     }
-    else if(e.target.value == "Days of Week") {
+    else if(e.target.name == "Days of Week") {
+      console.log('days of week')
       setDayType('day of week')
     }
+  }
+
+  const handleChange = (e:any) => {
+    for(let i of e) {
+      console.log(i.format())
+    }
+    setValue(e.value)
+    
   }
 
 
@@ -35,16 +69,35 @@ export default function MakeEvent() {
         </div>
 
         <div className="inline-flex justify-center  shadow-sm text-center" role="group">
-          <button onClick={handleDayChange} type="button" className="px-4 py-2 rounded-sm text-sm font-medium text-gray-900 bg-white border border-gray-200  hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+          <button name='Dates' onClick={handleDayChange} type="button" className="px-4 py-2 rounded-sm text-sm font-medium text-gray-900 bg-white border border-gray-200  hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
             Dates
           </button>
-          <button onClick={handleDayChange} type="button" className="px-4 py-2 rounded-sm text-sm font-medium text-gray-900 bg-white border-t border-b  border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+          <button name='Days of Week' onClick={handleDayChange} type="button" className="px-4 py-2 rounded-sm text-sm font-medium text-gray-900 bg-white border-t border-b  border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
             Days of Week
           </button>
         </div>
+
+
+      </div>
+      <div className='flex justify-center'> 
+        {dayType == 'day of week'? 
+          <Calendar 
+          className=' text-black'
+          multiple
+          onChange={handleChange}
+          /> 
+          :
+          <div className='flex justify-center'>
+            {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map((day, index) => (
+              <button name={day} onClick={handleDayOfWeek} key={index} className={`btn ${dayOfWeek.includes(day) ? 'bg-blue-400':'bg-blue-600'}  border p-2 `}>
+                {day}
+              </button>
+            ))}
+          </div>
+        }
         
       </div>
-
+        
 
     </div>
   )
